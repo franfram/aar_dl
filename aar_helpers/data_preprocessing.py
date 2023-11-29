d4b8caf5ad891198611c2f8b51658de3dc9dd33b
Francis Felici <francis.felici@gmail.com>
2023-11-29 15:48:40 +0000

    [ADD] prepare_training_data()

--- aar_helpers/data_preprocessing.py
+++ aar_helpers/data_preprocessing.py
@@ -1,12 +1,11 @@
-
-
 #import cudf as pd
 #import cupy as np
 import pandas as pd
 import numpy as np
 from typing import List, Dict, Tuple, Union
 from itertools import product
-
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
 
 
 
@@ -122,12 +121,8 @@ def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     df = compute_behaviours(df)
     return df
 
-
-
-
-
 # Function to extract consecutive segments
-def extract_consecutive_segments(df: pd.DataFrame, allowed_behaviours: List[str], sheep_name: str, month: int, behaviour_threshold: int=51, segment_size: int=256, sequence_length: int=5, move_window_by: str = 'segment') -> List[pd.DataFrame]:
+def extract_consecutive_segments(df: pd.DataFrame, allowed_behaviours: List[str], sheep_name: str, month: int, behaviour_threshold: int=51, segment_size: int=256, sequence_length: int=5, move_window_by: str = 'fraction') -> List[pd.DataFrame]:
     """
         Extracts sequences of consecutive segments of continuous data for a **specified sheep and month**,
     ensuring each segment in the sequence has at least a certain percentage of the same behaviour.
@@ -155,7 +150,7 @@ def extract_consecutive_segments(df: pd.DataFrame, allowed_behaviours: List[str]
     sequences = []
     i = 0
 
-    increments = {'segment': segment_size, 'franction': round(segment_size / 10), 'row': 1}
+    increments = {'segment': segment_size, 'fraction': round(segment_size / 10), 'row': 1}
     increment = increments.get(move_window_by)
     if increment is None: 
         raise ValueError("Invalid value for `move_window_by`. Must be one of 'segment', 'fraction', or 'row'.")
@@ -167,7 +162,7 @@ def extract_consecutive_segments(df: pd.DataFrame, allowed_behaviours: List[str]
         segments = [sequence.iloc[j*segment_size : (j+1)*segment_size].copy() for j in range(sequence_length)]
         valid_sequence = all(
             #segment['behaviours'].value_counts(normalize=True).iloc[0] * 100 >= behaviour_threshold
-            segment['behaviours'].value_counts(normalize=True).index[0] in allowed_behaviours and segment['behaviours'].value_counts(normalize=True).iloc[0] * 100 >= behaviour_threshold[0]
+            segment['behaviours'].value_counts(normalize=True).index[0] in allowed_behaviours and segment['behaviours'].value_counts(normalize=True).iloc[0] * 100 >= behaviour_threshold
             for segment in segments
         )
         
@@ -185,11 +180,7 @@ def extract_consecutive_segments(df: pd.DataFrame, allowed_behaviours: List[str]
     return sequences
 
 
-
-
-
-
-def extract_all_segments(df: pd.DataFrame, behaviour_threshold: List = [51], segment_size: List = [32, 64, 128, 256], sequence_length: List = [5, 10, 15, 20], move_window_by: str = 'segment') -> Dict[str, Dict[str, Dict[str, Union[int, List[pd.DataFrame]]]]]:
+def extract_all_segments(df: pd.DataFrame, allowed_behaviours: List[str] = ['Inactive', 'Walking', 'Foraging'], behaviour_threshold: Union[List, int] = [51], segment_size: Union[List, int] = [32, 64, 128, 256], sequence_length: Union[List, int] = [5, 10, 15, 20], move_window_by: str = 'segment') -> Dict[str, Dict[str, Dict[str, Union[int, List[pd.DataFrame]]]]]:
     """
     Extract sequences of segments for all unique sheep in the dataframe, given a combination of `behaviour_threshold`, 
     `segment_size`, and `sequence_length`.
@@ -199,13 +190,15 @@ def extract_all_segments(df: pd.DataFrame, behaviour_threshold: List = [51], seg
     
     Parameters:
     - df: DataFrame containing the sheep movement data.
+    - allowed_behaviours: The behaviours that are allowed in the sequences of segments, e.g. ['Inactive', 'Walking', 'Foraging'].
     - behaviour_threshold: Minimum percentage of rows with the same behaviour in each segment (default is 90).
     - segment_size: Number of rows in each segment (default is 256 for 6.4s of data at 40Hz).
     - sequence_length: Number of consecutive segments in each sequence (default is 20).
+    - move_window_by: The unit to move the window by when extracting segments, either 'segment', 'fraction' or 'row'. Function speeds up when moving by 'segment' compared to 'fraction' or 'row', but number of sequences extracted is (expectedly) lower.
     
     Returns:
     - A dictionary structured as:
-    {'BF<behaviour_threshold>_SS<segment_size>_SL<sequence_length>}: 
+    {'BF<behaviour_threshold>_SS<segment_size>_SL<sequence_length>'}: 
         {
             'sheep_name': {
                 'sequences': [list of sequences of segments],
@@ -218,9 +211,12 @@ def extract_all_segments(df: pd.DataFrame, behaviour_threshold: List = [51], seg
         ... 
     }
     """
-
     full_data = {}
 
+    # Ensure params are lists
+    behaviour_threshold, segment_size, sequence_length = [[p] if isinstance(p, int) else p for p in [behaviour_threshold, segment_size, sequence_length]]
+
+    # Extract sequences of segments
     for behaviour_threshold, segment_size, sequence_length in product(behaviour_threshold, segment_size, sequence_length): 
 
         segments_key = f"BT{behaviour_threshold}_SS{segment_size}_SL{sequence_length}"
@@ -230,18 +226,14 @@ def extract_all_segments(df: pd.DataFrame, behaviour_threshold: List = [51], seg
         for sheep in df['sheep_name'].unique(): 
             sequences = []
             for month in df['month'].unique(): 
-              #print(f"sheep: {sheep}")
-              #print(f"month: {month}")
-              
+              sequences.extend(extract_consecutive_segments(df, allowed_behaviours=allowed_behaviours, sheep_name=sheep, month=month, behaviour_threshold=behaviour_threshold, segment_size=segment_size, sequence_length=sequence_length, move_window_by=move_window_by))
 
-              sequences.extend(extract_consecutive_segments(df, sheep, month, behaviour_threshold, segment_size, sequence_length, move_window_by = move_window_by))
-
-              all_segments[sheep] = {
-                  'sequences': sequences, 
-                  'behaviour_threshold': behaviour_threshold,
-                  'segment_size': segment_size,
-                  'sequence_length': sequence_length
-              }
+            all_segments[sheep] = {
+                'sequences': sequences, 
+                'behaviour_threshold': behaviour_threshold,
+                'segment_size': segment_size,
+                'sequence_length': sequence_length
+            }
     
     
         if full_data.get(segments_key) == None:
@@ -250,19 +242,160 @@ def extract_all_segments(df: pd.DataFrame, behaviour_threshold: List = [51], seg
         else:
             print("There might be a problem")
 
+        total_sequences = sum(len(sheep_data['sequences']) for sheep_data in all_segments.values())
+        print(f"Total number of sequences for BF{behaviour_threshold}_SS{segment_size}_SL{sequence_length}: {total_sequences}")
+
     return full_data
 
 
 
 
 
+def check_for_nans(arr, label):
+    if np.isnan(np.array(arr)).any():
+        print(f"NaNs detected in {label}")
 
 
 
+"MUST THOROUGHLY CHECK THIS FUNCTION TO DETERMINE THE CAUSE OF THE SHAPE MISMATCHES"
+def prepare_training_data(
+    all_segments: Dict[str, Dict[str, Union[int, List[pd.DataFrame]]]], 
+    sequence_length: int, 
+    segment_size: int, 
+    features: List[str] = ['acc_x', 'acc_y', 'acc_z', 'mag_x', 'mag_y', 'mag_z', 'pitch.angle', 'roll.angle'], 
+    nan_strategy: str = 'interpolate', # 'mean, 'median', or 'interpolate'
+) -> Tuple[np.ndarray, np.ndarray, Dict]:
+    """
+    Prepare training data for the Convolutional + Transformer Neural Network model.
+
+    Parameters:
+    - all_segments: Dictionary containing the sequences of segments.
+    - features: List of feature names to be used.
+    - sequence_length: The number of consecutive segments in each sequence.
+    - segment_size: The number of rows in each segment.
+    - nan_strategy: The strategy to use for handling NaNs in the data.
+
+    Returns:
+    - x_train: Features data in the shape (n_sequences, sequence_length * segment_size, n_features).
+    - y_train: One-hot encoded behaviours in the shape (n_sequences, sequence_length, n_behaviours).
+    - behaviour_mapping: Mapping for the behaviour encoding
+    """
+
+    # Initialize lists to hold the training data
+    x_data = []
+    y_data = []
+
+    # Initialize OneHotEncoder for behaviours
+    encoder = OneHotEncoder(sparse=False)
 
 
+    # Collect all behaviour labels for fitting the encoder
+    all_behaviours = []
+
+    for sheep_data in all_segments.values():
+        for sequence in sheep_data['sequences']:
+            all_behaviours.extend(sequence['behaviour_majority_label'].unique())
+
+    # Fit the encoder on all unique behaviours
+    encoder.fit(np.array(all_behaviours).reshape(-1, 1))
+
+    # Get the behaviour mapping
+    behaviour_mapping = {category: idx for idx, category in enumerate(encoder.categories_[0])}
+
+    # Choose the imputation strategy
+    if nan_strategy in ['mean', 'median']:
+        imputer = SimpleImputer(strategy=nan_strategy)
+
+    # Choose post imputation strategy
+    post_imputer = SimpleImputer(strategy='mean')
+
+    # Extract features and one-hot encode behaviours for each sequence
+    for sheep_data in all_segments.values():
+        for sequence_idx, sequence in enumerate(sheep_data['sequences']):
+            # Check if all features are present
+            if not all(feature in sequence.columns for feature in features):
+                missing_features = [feature for feature in features if feature not in sequence.columns]
+                print(f"Missing features")# {missing_features} in sheep {sequence['sheep_name'].unique()}, sequence {sequence_idx}")
+                continue  
+
+            # Extract features
+            sequence_features = sequence[features].values
+
+            # Find the indices of NaNs before imputation
+            #nan_indices = np.argwhere(np.isnan(sequence_features))
 
 
+            # POST NAN CHECKING
+            # Apply the chosen NaN handling strategy: 
+            if nan_strategy in ['mean', 'median']:
+                # Check if any columns only contain NaNs
+                nan_cols = np.all(np.isnan(sequence_features), axis=0)
+                if np.any(nan_cols):
+                    print(f"Columns {np.where(nan_cols)} only contain NaNs.")
+
+                # Calculate and print the percentage of NaNs
+                nan_percentage = np.isnan(sequence_features).mean() * 100
+                print(f"Percentage of NaNs when strategy is mean or median: {nan_percentage}%")
+
+
+                # Impute NaNs
+                sequence_features = imputer.fit_transform(sequence_features)
+
+
+            elif nan_strategy == 'interpolate':
+                # Check where NaNs are 
+                sequence_features_check = pd.DataFrame(sequence_features)
+                if sequence_features_check.isnull().values.any():
+                    print("NaNs detected in sequence_features at:")
+                    print(np.where(sequence_features_check.isnull()))
+
+                # Calculate and print the percentage of NaNs
+                #nan_percentage = sequence_features.isnull().mean().mean() * 100
+                nan_percentage = np.isnan(sequence_features).mean().mean() * 100
+                print(f"Percentage of NaNs when strategy is interpolate: {nan_percentage}%")
+
+                # Impute NaNs
+                #sequence_features = pd.DataFrame(sequence_features).interpolate(method='linear', axis=0).fillna(method='bfill').fillna(method='ffill').values
+                sequence_features = pd.DataFrame(sequence_features).interpolate(method='linear', axis=0).bfill().ffill().values
+
+
+            # PRE NAN CHECKING
+            ## Apply the chosen NaN handling strategy: 
+            #if nan_strategy in ['mean', 'median']:
+                #sequence_features = imputer.fit_transform(sequence_features)
+            #elif nan_strategy == 'interpolate':
+                #sequence_features = pd.DataFrame(sequence_features).interpolate(method='linear', axis=0).fillna(method='bfill').fillna(method='ffill').values
+
+           
+            # Print the locations of imputed NaNs
+            #for nan_idx in nan_indices:
+                #feature_idx = nan_idx
+
+            reshaped_features = sequence_features.reshape(sequence_length * segment_size, len(features))
+            reshaped_features = post_imputer.fit_transform(reshaped_features)
+
+            # Check the shape of reshaped_features
+            if reshaped_features.shape != (sequence_length * segment_size, len(features)):
+                print(f"Sequence {sequence_idx} will be dropped due to Shape mismatch in reshaped_features: {reshaped_features.shape} for sheep {sequence['sheep_name'].unique()}, sequence {sequence_idx}.\n ToDo: Try to handle these shape mismatches")
+                continue  # Skip this sequence or handle it differently
+
+
+            x_data.append(reshaped_features)
+            check_for_nans(reshaped_features, label="reshaped_features")
+
+
+            # One-hot encode behaviours
+            behaviours = sequence['behaviour_majority_label'].iloc[::segment_size].values.reshape(-1, 1)
+            encoded_behaviours = encoder.transform(behaviours)
+            y_data.append(encoded_behaviours)
+            check_for_nans(encoded_behaviours, label="encoded_behaviours after transformation")
+
+    
+    # Convert lists to numpy arrays
+    x_data: np.ndarray = np.array(x_data)
+    y_data: np.ndarray = np.array(y_data)
+    
+    return x_data, y_data, behaviour_mapping
 
 
 
